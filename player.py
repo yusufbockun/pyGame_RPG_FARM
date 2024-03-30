805c4f63944ca23e49c3f3c7ea1ec37ef300b735
Yusuf <yusufbockun1@gmail.com>
2024-03-31 02:12:08 +0300

    without tools and map

---
+++ player.py
@@ -0,0 +1,82 @@
+import pygame
+from support import *
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        # General Setup
+        self.import_assets()
+        self.status='down_idle'
+        self.frame_index=0
+        
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
+
+        # Movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+        
+        self.selectedTool='axe'
+        
+    def import_assets(self):
+        self.animations={'up':[],'down':[],'left':[],'right':[],
+                         'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+                         'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
+                         'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
+                         'right_water':[],'left_water':[],'up_water':[],'down_water':[]}
+        for animation in self.animations.keys():
+            full_path='graphics/character/'+animation
+            self.animations[animation]=import_folder(full_path)
+        
+    def animate(self,dt):
+        self.frame_index+=4*dt
+        if self.frame_index>=len(self.animations[self.status]):
+            self.frame_index=0
+        self.image=self.animations[self.status][int(self.frame_index)]
+    def input(self):
+        keys = pygame.key.get_pressed()
+        # Movement
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+            self.status='up'
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+            self.status='down'
+        else:
+            self.direction.y = 0
+            
+            
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+            self.status='left'
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+            self.status='right'
+        else:
+            self.direction.x = 0
+
+
+    def move(self, dt):
+    
+        if self.direction.magnitude()>0:
+            self.direction=self.direction.normalize()
+            
+        #horizontal movement
+        self.pos.x+= self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        #vertical movement
+        self.pos.y+= self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+        
+    def get_status(self):
+        #animation idle
+        if self.direction.magnitude()==0:
+            self.status= self.status.split('_')[0]+'_idle'
+        
+        #animation tools
+    def update(self, dt):
+        self.input()
+        self.get_status()
+        self.move(dt)
+        self.animate(dt)
